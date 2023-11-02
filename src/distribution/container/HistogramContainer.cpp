acb75c429e73fe953c30806237ec04ca475407d0
DanYuDE <nadal3815@gmail.com>
2023-11-02 15:09:50 +0100

    Update histogram container

---
+++ src/distribution/container/HistogramContainer.cpp
@@ -0,0 +1,39 @@
+/*
+ * HistogramContainer.cpp
+ *
+ *  Created on: Oct 31, 2023
+ *      Author: dan
+ */
+
+#include "HistogramContainer.h"
+#include "inet/common/XMLUtils.h"
+#include "inet/networklayer/configurator/base/L3NetworkConfiguratorBase.h"
+
+namespace d6g {
+
+using namespace inet::xmlutils;
+
+Define_Module(HistogramContainer);
+
+void HistogramContainer::initialize(int stage) {
+    if (stage == INITSTAGE_LOCAL) {
+        // Get the histogram configurations from the parameters
+        std::map<std::string, cXMLElement*> histogramConfigs = par("histograms").xmlValue();
+        for (const auto& [name, config]: histogramConfigs) {
+            Histogram* histogram = new Histogram();
+            histogram->Histogram::parseHistogramConfig(config);  // You might need to add this method to your Histogram class
+            histograms[name] = histogram;
+        }
+    }
+}
+
+Histogram* HistogramContainer::getHistogram(const std::string& name) {
+    auto it = histograms.find(name);
+    if (it != histograms.end()) {
+        return it->second;
+    } else {
+        throw cRuntimeError("Histogram not found: %s", name.c_str());
+    }
+}
+
+} /* namespace d6g */
