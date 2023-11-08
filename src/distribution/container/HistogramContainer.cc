74bd44140d72ab88a2beae72fa3e27c095bfdca5
CharlesGuo16 <18818201750@163.com>
2023-11-08 19:36:20 +0100

    updated the variable of xmlData

--- src/distribution/container/HistogramContainer.cc
+++ src/distribution/container/HistogramContainer.cc
@@ -39,7 +39,40 @@ Histogram* HistogramContainer::getHistogram(const char* xmlName) {
         throw cRuntimeError("File '%s' not found", xmlName);
     }
 
-    cXMLElement* xmlData = par(xmlName).xmlValue();
+
+
+    cXMLElement* xmlData;
+    //may needed to make it more simply
+    std::string line;
+    while (std::getline(xmlFile, line)) {
+           // Process each line of the XML file here
+           if (line.find('<') != std::string::npos && line.find('>') != std::string::npos) {
+
+               // Extract the element name and content
+               size_t start = line.find('<') + 1;
+               size_t end = line.find('>');
+
+               if (start != std::string::npos && end != std::string::npos) {
+                   xmlData.key = line.substr(start, end - start);
+
+                   size_t contentStart = end + 1;
+                   size_t contentEnd = line.find('<', contentStart);
+                   if (contentEnd != std::string::npos) {
+                       xmlData.value = line.substr(contentStart, contentEnd - contentStart);
+                   }
+               }
+           }
+       }
+    //Is xmlData an array, a link list or something else? it should store all xml elements in xml file, rather than only one element
+
+
+
+
+
+
+
+
+    //cXMLElement* xmlData = par(xmlName).xmlValue();
     if (!xmlData || strcmp(xmlData->getTagName(), "histogram") != 0) {
             throw cRuntimeError("Invalid XML data for histogram");
         }
